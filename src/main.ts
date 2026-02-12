a75882eb075e69b70525b43f2929bde8cfac1978
Taha3301 <taha.daoud00@gmail.com>
2026-02-12 15:24:00 +0100

    Configure CORS for all origins and bind to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: true,
     credentials: true,
   });
 
@@ -28,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
